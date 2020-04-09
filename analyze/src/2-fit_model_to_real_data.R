edb7ab868af4569bfe68fc8e70e0dec63a23b1ae
Patrick Ball <pball@hrdag.org>
2020-04-08 18:31:01 -0700

    stylistic edits to 2, 3, including Makefile

--- analyze/src/2-fit_model_to_real_data.R
+++ analyze/src/2-fit_model_to_real_data.R
@@ -1,29 +1,29 @@
 #
 # Authors:     KL
 # Maintainers: JJ, PB, MG
-# Copyright:  
+# Copyright:
 # =========================================
 # Covid19-modeling/analyze/src/2-fit_model_to_real_data.R
 
 #this code fits a model to the data
 
 if (!require('pacman')) {install.packages('pacman')}
-pacman::p_load(ggplot2, dplyr, tidyr, readr, lubridate, 
+pacman::p_load(ggplot2, dplyr, tidyr, readr, lubridate,
                future, future.apply, here, glue, argparse)
 
 parser <- ArgumentParser()
 parser$add_argument('--functions', default=here::here('analyze/src/functions.R'))
 parser$add_argument('--theta', default=here::here('analyze/output/theta.Rds'))
 parser$add_argument('--pops', default=here::here('analyze/input/country_pops.RData'))
-parser$add_argument('--worldometers', default=here::here('analyze/input/worldounts_deaths_us.csv'))
+parser$add_argument('--worldometers', default=here::here('analyze/input/worldcounts_deaths_us.csv'))
 parser$add_argument('--p', default=0.01)
-parsre$add_argument('--country', default='us')
+parser$add_argument('--country', default='us')
 
 args <- parser$parse_args()
 
-args[['mcmc']] <- here::here(glue('analyze/output/mcmc_run_{p}.RData'))
-args[['bigDs']] <- here::here(glue('analyze/output/bigDs_{p}.RData'))
-args[['bigNus']] <- here::here(glue('analyze/output/bigNus_{p}.RData'))
+args[['mcmc']] <- here::here(glue('analyze/output/mcmc_run_{args$p}.RData'))
+args[['bigDs']] <- here::here(glue('analyze/output/bigDs_{args$p}.RData'))
+args[['bigNus']] <- here::here(glue('analyze/output/bigNus_{args$p}.RData'))
 
 source(args$functions)
 
@@ -98,13 +98,13 @@ save(file=args$mcmc, mcmc.out)
 ##post-process MCMC output to be more easily plotted.... in next script (this takes a while to run, like the mcmc)
 NUS <- mcmc.out$NUS
 tmp <- simulate.sir(N,
-                    mean(mcmc.out$BETA), 
+                    mean(mcmc.out$BETA),
                     mean(mcmc.out$GAMMAR),
-                    I, 
+                    I,
                     T,
-                    max.time, 
-                    theta, 
-                    p, 
+                    max.time,
+                    theta,
+                    p,
                     floor(mean(mcmc.out$T0S)))
 plot(tmp$Ds[1:T], Ds)
 abline(0, 1)
@@ -124,25 +124,25 @@ plan(multisession)
 BGt <- split(cbind(BETA.thinned, GAMMAR.thinned, T0.thinned), seq(1:n.thin))
 
 #for each mcmc sample simulate sir model under those model parameters; times are integers so use T0 as floor of estimated  T0,
-tmp1 <- future_lapply(BGt, function(x){simulate.sir(N, 
-                                                    x[1], 
-                                                    x[2], 
-                                                    I, 
-                                                    T + n.days.future, 
+tmp1 <- future_lapply(BGt, function(x){simulate.sir(N,
+                                                    x[1],
+                                                    x[2],
+                                                    I,
+                                                    T + n.days.future,
                                                     max.time,
-                                                    theta, 
-                                                    p, 
+                                                    theta,
+                                                    p,
                                                     floor(x[3]))})
 
 #for each mcmc sample simulate sir model under those model parameters; times are integers so use T0 as ceiling of estimated  T0,
-tmp2 <- future_lapply(BGt, function(x){simulate.sir(N, 
-                                                    x[1], 
-                                                    x[2], 
+tmp2 <- future_lapply(BGt, function(x){simulate.sir(N,
+                                                    x[1],
+                                                    x[2],
                                                     I,
-                                                    T + n.days.future, 
+                                                    T + n.days.future,
                                                     max.time,
-                                                    theta, 
-                                                    p, 
+                                                    theta,
+                                                    p,
                                                     ceiling(x[3]))})
 
 #extract the samples of number of deaths under this model
@@ -156,40 +156,40 @@ big.Ds[,,n.cases+1] <- (tmp01 * (T0.thinned - floor(T0.thinned))
 tmp01 <- sapply(tmp1, function(x){return(x$Nu[1:(T + n.days.future)])})
 tmp02 <- sapply(tmp2, function(x){return(x$Nu[1:(T + n.days.future)])})
 #save the extracted Nus
-big.Nus[,,n.cases+1] <- (tmp01 * (T0.thinned - floor(T0.thinned)) 
+big.Nus[,,n.cases+1] <- (tmp01 * (T0.thinned - floor(T0.thinned))
                          + tmp02 * (ceiling(T0.thinned) - T0.thinned)) #put simulation with actual parameters in last position
 
 days.since.mitigation <- 10
 
 for (k in 1:n.cases) {
   print(k)
-  tmp1 <- future_lapply(BGt, function(x){simulate.sir(N, 
+  tmp1 <- future_lapply(BGt, function(x){simulate.sir(N,
                                                       c(rep(x[1], T - days.since.mitigation),
-                                                        rep(x[2], days.since.mitigation + n.days.future) * R0.case[k]), #sample SIR as  though the R0 decreased by R0.case[k] days.since.mitigation ago
-                                                      x[2], 
-                                                      I, 
-                                                      T + n.days.future, 
-                                                      max.time, 
-                                                      theta, 
-                                                      p, 
+                                                        rep(x[2], days.since.mitigation + n.days.future) * R0.case[k]), # sample SIR as  though the R0 decreased by R0.case[k] days.since.mitigation ago
+                                                      x[2],
+                                                      I,
+                                                      T + n.days.future,
+                                                      max.time,
+                                                      theta,
+                                                      p,
                                                       floor(x[3]))})
   tmp2 <- future_lapply(BGt, function(x){simulate.sir(N,
                                                       c(rep(x[1], T - days.since.mitigation),
                                                         rep(x[2], days.since.mitigation + n.days.future) * R0.case[k]),
-                                                      x[2], 
-                                                      I, 
-                                                      T + n.days.future, 
-                                                      max.time, 
-                                                      theta, 
-                                                      p, 
+                                                      x[2],
+                                                      I,
+                                                      T + n.days.future,
+                                                      max.time,
+                                                      theta,
+                                                      p,
                                                       ceiling(x[3]))})
-  
+
   #extract samples of deaths
   tmp01 <- sapply(tmp1, function(x){return(x$Ds[1:(T + n.days.future)])})
   tmp02 <- sapply(tmp2, function(x){return(x$Ds[1:(T + n.days.future)])})
-  big.Ds[,,k] <- (tmp01 * (T0.thinned - floor(T0.thinned)) 
+  big.Ds[,,k] <- (tmp01 * (T0.thinned - floor(T0.thinned))
                   + tmp02 * (ceiling(T0.thinned) - T0.thinned))
-  
+
   #extract Nus
   tmp01 <- sapply(tmp1, function(x){return(x$Nus)})
   tmp02 <- sapply(tmp2, function(x){return(x$Nus)})
--- analyze/src/3-make_figures.R
+++ analyze/src/3-make_figures.R
@@ -1,38 +1,37 @@
 #
 # Authors:     KL
 # Maintainers: JJ, PB, MG
-# Copyright:  
+# Copyright:
 # =========================================
 # Covid19-modeling/analyze/src/2-fit_model_to_real_data.R
 
 #simulate data for testing code
 if (!require('pacman')) {install.packages('pacman')}
 pacman::p_load(ggplot2, dplyr, tidyr, readr, lubridate,
-               future, future.apply, reshape2, here)
+               glue, future, future.apply, reshape2, argparse, here)
 
 parser <- ArgumentParser()
 parser$add_argument('--functions', default=here::here('analyze/src/functions.R'))
 parser$add_argument('--theta', default=here::here('analyze/output/theta.Rds'))
 parser$add_argument('--pops', default=here::here('analyze/input/country_pops.RData'))
-parser$add_argument('--worldometers', default=here::here('analyze/input/worldounts_deaths_us.csv'))
+parser$add_argument('--worldometers', default=here::here('analyze/input/worldcounts_deaths_us.csv'))
 parser$add_argument('--p', default=0.01)
-parsre$add_argument('--country', default='us')
+parser$add_argument('--country', default='us')
 
 args <- parser$parse_args()
 
-args[['mcmc']] <- here::here(glue('analyze/output/mcmc_run_{p}.Rdata'))
-args[['trace_plots']] <- here::here(glue('analyze/output/trace_plots_fractional_{country}_{p}.png'))
-args[['post_hists']] <- here::here(glue('analyze/output/post_hists_fractional_{country}_{p}.png'))
-args[['bigDs']] <- here::here(glue('analyze/output/bigDs_{p}'))
-args[['bigNus']] <- here::here(glue('analyze/output/bigNus_{p}'))
-args[['num_deaths_cases']] <- here::here(glue('analyze/output/num_deaths_cases_fractional_{country}_{p}.png'))
-args[['num_newly_infected']] <- here::here(glue('analyze/output/num_newly_infected_cases_fractional_{country}_{p}.png'))
-args[['predicted_vs_actual']] <- here::here(glue('analyze/output/Predicted_vs_actual_fractional_{country}_{p}.png'))
-args[['R0']] <- here::here(glue('analyze/output/R0_fractional_{country}_{p}.png'))
-args[['infected_10']] <- here::here(glue('analyze/output/infected_10_days_ago_fractional_{country}_{p}.png'))
-args[['newly_infected_10']] <- here::here(glue('analyze/output/newly_infected_10_days_ago_fractional_{country}_{p}.png'))
-args[['time_to_recovery']] <- here::here(glue('analyze/output/time_to_recovery_fractional_{country}_{p}.png'))
-
+args[['mcmc']] <- here::here(glue('analyze/output/mcmc_run_{args$p}.Rdata'))
+args[['trace_plots']] <- here::here(glue('analyze/output/trace_plots_fractional_{args$country}_{args$p}.png'))
+args[['post_hists']] <- here::here(glue('analyze/output/post_hists_fractional_{args$country}_{args$p}.png'))
+args[['bigDs']] <- here::here(glue('analyze/output/bigDs_{args$p}.Rdata'))
+args[['bigNus']] <- here::here(glue('analyze/output/bigNus_{args$p}.Rdata'))
+args[['num_deaths_cases']] <- here::here(glue('analyze/output/num_deaths_cases_fractional_{args$country}_{args$p}.png'))
+args[['num_newly_infected']] <- here::here(glue('analyze/output/num_newly_infected_cases_fractional_{args$country}_{args$p}.png'))
+args[['predicted_vs_actual']] <- here::here(glue('analyze/output/Predicted_vs_actual_fractional_{args$country}_{args$p}.png'))
+args[['R0']] <- here::here(glue('analyze/output/R0_fractional_{args$country}_{args$p}.png'))
+args[['infected_10']] <- here::here(glue('analyze/output/infected_10_days_ago_fractional_{args$country}_{args$p}.png'))
+args[['newly_infected_10']] <- here::here(glue('analyze/output/newly_infected_10_days_ago_fractional_{args$country}_{args$p}.png'))
+args[['time_to_recovery']] <- here::here(glue('analyze/output/time_to_recovery_fractional_{args$country}_{args$p}.png'))
 
 
 source(args$functions)
@@ -46,7 +45,7 @@ theta <- readRDS(args$theta)
 pops <- readRDS(args$pops)
 
 max.time <- length(theta) - 1
-N <- pops[rownames(pops) == country]
+N <- pops[rownames(pops) == args$country]
 
 days.since.mitigation <- 10
 
@@ -60,8 +59,8 @@ dat <- dat %>% mutate(date=mdy(date))
 Ds <- dat$deaths_us
 Ds <- c(rep(0, 14), Ds)
 
-dt.all <- c(min(dat$date) + seq(from=-14, to=-1, by=1), 
-            dat$date, 
+dt.all <- c(min(dat$date) + seq(from=-14, to=-1, by=1),
+            dat$date,
             max(dat$date) + seq(21))
 
 # In a "normal" SIR model, the R0 is beta/gamma
@@ -85,27 +84,29 @@ df.bg <- data.frame(beta=mcmc.out$BETA,
                     gammar=mcmc.out$GAMMAR,
                     T0=mcmc.out$T0S,
                     iter=seq(nmc))
-df.bg <- df.bg %>% 
-  mutate(R0=beta / gammar) %>% 
+df.bg <- df.bg %>%
+  mutate(R0=beta / gammar) %>%
   gather(variable, value, -iter)
 
 png(args$trace_plots, width=600, height=400)
-ggplot(df.bg, aes(x=iter, y=value)) + 
-  geom_point(size=0.4) + 
+ggplot(df.bg, aes(x=iter, y=value)) +
+  geom_point(size=0.4) +
   facet_wrap(~variable, scales='free') +
   theme(text=element_text(size=24), axis.text.x=element_text(angle=90)) +
   xlab("iteration") +
   theme_bw()
 dev.off()
+print("traceplot done")
 
 #make histograms of posterior distribution of parameters
-png(args$post_hists, with=600, height=400)
+png(args$post_hists, width=600, height=400)
 ggplot(df.bg %>% filter(iter>=burn), aes(x=value)) +
   geom_histogram() +
-  facet_wrap(~variable, scales='free') + 
+  facet_wrap(~variable, scales='free') +
   theme(text=element_text(size=24), axis.text.x=element_text(angle=90)) +
   theme_bw()
 dev.off()
+print("posterior dists done")
 
 #load post-processed MCMC output
 load(file=args$bigDs)
@@ -132,13 +133,13 @@ df.qlo$day <- dt.all
 df.qlo <- df.qlo %>% gather(R0, q025, -day)
 
 df.qhi <- data.frame(qhi.Ds)
-names(df.qhi) <- c(R0.case, 
+names(df.qhi) <- c(R0.case,
                    round(mean(mcmc.out$BETA[burn:nmc]/mcmc.out$GAMMAR[burn:nmc]), 2))
 #df.qhi$day <- seq(T+n.days.future)
 df.qhi$day <- dt.all
 df.qhi <- df.qhi %>% gather(R0, q975, -day)
 
-df.Ds <- df.Ds %>% 
+df.Ds <- df.Ds %>%
   inner_join(df.qlo, by=c('R0', 'day')) %>%
   inner_join(df.qhi, by=c('R0','day'))
 df.Ds$R0 <- factor(df.Ds$R0)
@@ -146,12 +147,12 @@ df.Ds$R0 <- factor(df.Ds$R0)
 #plot future trajectories under different scenarios for R0
 png(args$num_deaths_cases, width=600, height=400)
 
-ggplot(df.Ds %>% 
+ggplot(df.Ds %>%
          filter(day > date("2020-03-28") & day <= date("2020-03-28") + n.days.future),
        aes(x=day, y=log10(value), col=R0)) +
   geom_line() +
   theme_bw() +
-  geom_ribbon(data=df.Ds %>% 
+  geom_ribbon(data=df.Ds %>%
                 filter(day > date("2020-03-28") & day <= date("2020-03-28") + n.days.future),
               aes(ymin=log10(q025),
                   ymax=log10(q975),
@@ -162,6 +163,7 @@ ggplot(df.Ds %>%
   ylim(c(0, 5)) +
   xlab("date")
 dev.off()
+print("future trajectories done")
 
 #get more quantiles
 mean.Nus <- apply(big.Nus, c(1, 3), mean)
@@ -187,29 +189,30 @@ df.qhi$day <- dt.all
 #df.qhi$day <- seq(T+n.days.future)
 df.qhi <- df.qhi %>% gather(R0, q975, -day)
 
-df.Nus <- df.Nus %>% 
+df.Nus <- df.Nus %>%
   inner_join(df.qlo, by=c('R0', 'day')) %>%
   inner_join(df.qhi, by=c('R0', 'day'))
-df.Nus <- df.Nus %>% 
+df.Nus <- df.Nus %>%
   mutate(ribbon = day > date("2020-03-20") | (R0 > 2.5 &  day > date("2020-03-15")))
 df.Nus$R0 <- factor(df.Nus$R0)
 
 #plot future new infections under different scenarios for reductions in R0
 png(args$num_newly_infected, width=600, height=400)
-ggplot(df.Nus %>% 
-         filter((day > date("2020-03-20") - days.since.mitigation &
-                   day <= date("2020-03-29") + n.days.future)),
+ggplot(df.Nus %>% filter(
+						 (day > date("2020-03-20") - days.since.mitigation &
+						  day <= date("2020-03-29") + n.days.future)),
        aes(x=day, y=log10(value+1), col=R0)) +
   geom_line() +
   theme_bw() +
   theme(text=element_text(size=24)) +
-  geom_ribbon(df.Nus %>% 
+  geom_ribbon(df.Nus %>%
                 filter((day > date("2020-03-20") - days.since.mitigation &
                           day <= date("2020-03-29") + n.days.future)),
-              aes(ymin=log10(q025), ymax=log10(q975), fill=R0), alpha=0.2) +
+              mapping = aes(ymin=log10(q025), ymax=log10(q975), fill=R0), alpha=0.2) +
   xlab("date") +
   ylab(expression(paste(log[10], nu)))
 dev.off()
+print("future new infections done.")
 
 big.Ds.all <- big.Ds
 big.Ds <- big.Ds[1:T,,n.cases+1]
@@ -218,19 +221,19 @@ big.Ds <- data.frame(big.Ds)
 #big.Ds$time <- 1:T
 big.Ds$date <- dt.all[1:T]
 #big.Ds.long <- big.Ds %>% gather(variable,value,-time)  #melt(big.Ds, id.vars=c("time"))
-big.Ds.long <- melt(big.Ds, id.vars=c("date")) 
+big.Ds.long <- melt(big.Ds, id.vars=c("date"))
 big.Ds.long$alpha <- 0.05
 big.Ds.long$type <- "simulations"
-means <- data.frame(date=dt.all[1:T], 
+means <- data.frame(date=dt.all[1:T],
                     variable="mean",
-                    value=apply(big.Ds[,1:length(thinned)], 1, mean), 
+                    value=apply(big.Ds[,1:length(thinned)], 1, mean),
                     alpha=1,
                     type = "mean")
 big.Ds.long <- rbind(big.Ds.long, means)
 data <- data.frame(date=dt.all[1:T],
-                   variable="data", 
-                   value=Ds, 
-                   alpha=5, 
+                   variable="data",
+                   value=Ds,
+                   alpha=5,
                    type="actual deaths")
 big.Ds.long <- rbind(big.Ds.long, data)
 
@@ -241,11 +244,12 @@ ggplot(filter(big.Ds.long, date > date("2020-02-15")),
        aes(x=date, y=value, group=variable, alpha=alpha, color=type)) +
   geom_line() +
   scale_color_manual(values=c("red", "blue", "gray")) +
-  guides(alpha=FALSE) + 
+  guides(alpha=FALSE) +
   theme_bw() +
   theme(text=element_text(size=24))
 #geom_line(aes(x=time,y=variable), data=filter(big.Ds.long, type=="actual deaths"))
 dev.off()
+print("pred vs actual done")
 
 R0 <- data.frame(r=mcmc.out$BETA / mcmc.out$GAMMAR)
 R0 <- R0 %>% mutate(iter=seq(nmc))
@@ -258,42 +262,46 @@ ggplot(R0 %>% filter(iter > burn), aes(x=r)) +
   theme(text=element_text(size=24)) +
   theme_bw()
 dev.off()
+print("R0 histogram done")
 
 
-df1 <- data.frame(total_infections=apply(mcmc.out$NUS[(burn + 1):nmc, 1:(T - 10)], 
-                                         1, 
+df1 <- data.frame(total_infections=apply(mcmc.out$NUS[(burn + 1):nmc, 1:(T - 10)],
+                                         1,
                                          sum))
 png(args$infected_10, width=600, height=400)
-ggplot(df1, aes(x=total_infections)) + 
-  geom_histogram() + 
+ggplot(df1, aes(x=total_infections)) +
+  geom_histogram() +
   theme_bw() +
   xlab("Infected + Recovered") +
   theme(plot.title=element_text(hjust=0.5)) +
   theme(text=element_text(size=24))
 dev.off()
+print("inf + recovered done")
 
 df1 <- data.frame(new_infections=mcmc.out$NUS[(burn+1):nmc,T-10])
 
 png(args$newly_infected_10, width=600, height=400)
 ggplot(df1, aes(x=new_infections)) +
-  geom_histogram() + 
+  geom_histogram() +
   theme_bw() +
   xlab("New Infected") +
   theme(plot.title=element_text(hjust=0.5)) +
   theme(text=element_text(size=24))
 dev.off()
+print("newly infected done")
 
-png(args$time_to_recovery, width=600, hieght=400)
+png(args$time_to_recovery, width=600, height=400)
 hist(1 / mcmc.out$GAMMAR[(burn+1):nmc],
      main='posterior distribution of \n average length of the infectious period',
      xlab=expression(gamma[r]^(-1)))
 dev.off()
+print("time to recovery")
 
 quantile(R0$r[R0$iter > burn], c(0.025, 0.975))
 
-df.Ds %>% 
-  filter(day %in% c(date("2020-03-29"), date("2020-03-30"), date("2020-03-31"))) %>% 
-  group_by(day) %>% 
+df.Ds %>%
+  filter(day %in% c(date("2020-03-29"), date("2020-03-30"), date("2020-03-31"))) %>%
+  group_by(day) %>%
   summarise(mn.deaths=mean(value))
 
 # done.
